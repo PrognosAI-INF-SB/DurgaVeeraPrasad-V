fd62985857aa9c90409548de45d143e602fb9056
PrasadVdv <prasad.vdv1990@gmail.com>
2025-10-13 07:11:38 +0530

    Delete Milestone-3 directory

--- Milestone-3/src/config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-FILE_PATH = r"C:\Users\DELL\OneDrive\Desktop\Final Project\Milestone3 Durga Veera Prasad V\Milestone3 Durga Veera Prasad V\Milestone3 Durga Veera Prasad V\Rul-Predictive-Maintenance\data\train_FD001.csv"
-OUTPUT_FILE = r"C:\Users\DELL\OneDrive\Desktop\Final Project\Milestone3 Durga Veera Prasad V\Milestone3 Durga Veera Prasad V\Milestone3 Durga Veera Prasad V\Rul-Predictive-Maintenance\data\train_FD001_processed.npz"
-WINDOW_SIZE = 30
